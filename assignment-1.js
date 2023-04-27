327d1036dfd814832662c792efc98d44a22bb3e0
Adi <adi@macbook.local>
2023-04-27 11:07:29 -0600

    First commit with assignment 1 (recursion)

---
+++ assignment-1.js
@@ -0,0 +1,22 @@
+function fibs(num) {
+  const arr = [];
+  for (let i = 0; i < num; i++) {
+    i === 0
+      ? arr.push(0)
+      : i === 1 || i === 2
+        ? arr.push(1)
+        : arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+  }
+  return arr;
+}
+
+function fibsRec(start, num) {
+  return start < num
+    ? [fibs(start + 1)[fibs(start + 1).length - 1]].concat(
+      fibsRec(start + 1, num),
+    )
+    : [];
+}
+
+console.log(`Using iteration: ${fibs(8)}`);
+console.log(`Using recursion: ${fibsRec(0, 8)}`);
